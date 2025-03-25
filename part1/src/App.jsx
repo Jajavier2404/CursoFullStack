84262b83f7cabf1219c8806622b31c619d800b33
Jajavier2404 <javialex2008@gmail.com>
2025-03-24 21:13:52 -0500

    hola

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,13 +1,25 @@
-function App() {
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-  console.log(now,a+b);
+import Header from './components/Header'
+import Content from './components/Content'
+import Total from './components/Total'
+
+const App = () => { 
+  
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
   return (
-    <>
-      <h1>Hola Desarrollador son las, {now.toString()}</h1>
-      <p>{a} + {b} es igual a {a+b}</p>
-    </>
+    <div>
+      <Header course={course} />
+      <Content part1={part1} exercises1={exercises1} 
+                part2={part2} exercises2={exercises2} 
+                part3={part3} exercises3={exercises3} />
+      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+    </div>
   )
 }
 
